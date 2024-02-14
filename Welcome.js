b74f19423fc12758b9961a14c576fb5013e66b72
lakshmi-prasanna345 <majjigalakshmi676@gmail.com>
2024-02-14 19:34:29 +0530

    my first commit

--- Welcome.js
+++ Welcome.js
@@ -2,7 +2,7 @@ import React, {Component} from "react";
 
 class Welcome extends Component{
     render(){
-        return <h1>welcome.class component</h1>
+        return <h1>welcome to the class component</h1>
         {this.props.children}
     }
 }
